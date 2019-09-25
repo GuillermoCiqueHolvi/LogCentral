7836285b6c0df7e1dda51bd6343d1c8794314dd7
Guillermo Cique <Guillermo@holvi.com>
2019-09-25 12:34:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,11 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "LogCentral",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_12)
+    ],
     products: [
         .library(name: "LogCentral", targets: ["LogCentral"])
     ],
     targets: [
         .target(name: "LogCentral", path: "Sources"),
         .testTarget(name: "LogCentralTests", dependencies: ["LogCentral"], path: "Tests")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
